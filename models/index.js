9d3abce0380f2bbef25fd915a74ec5d71b8220c7
micah_z <micah.a.zahn@gmail.com>
2022-06-02 18:33:04 -0500

    initial models file setup

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Review = require('./Review');
+const Comment = require('./Comment');
+const Book = require('./Book');
+
+module.exports = { User, Review, Comment, Book }
