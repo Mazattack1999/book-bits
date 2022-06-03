8e6dd4a0a4bdf91902707d4c0e4d521cdc75bfc2
Torgy <jctorge@gmail.com>
2022-06-02 21:21:52 -0500

    email requirement removed/further file structer added

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,10 +1,10 @@
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
+    const userName = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
+    if (userName && password) {
       const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
@@ -15,7 +15,7 @@ async function loginFormHandler(event) {
       });
   
       if (response.ok) {
-        document.location.replace('/dashboard/');
+        document.location.replace('/myposts/');
       } else {
         alert(response.statusText);
       }
@@ -26,22 +26,20 @@ async function loginFormHandler(event) {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && email && password) {
+    if (username && password) {
       const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
           username,
-          email,
           password
         }),
         headers: { 'Content-Type': 'application/json' }
       });
   
       if (response.ok) {
-        document.location.replace('/dashboard/');
+        document.location.replace('/myposts/');
       } else {
         alert(response.statusText);
       }
