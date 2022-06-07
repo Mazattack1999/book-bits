f6c9734d1d9d2c62c2de8d7151b54fc99dfc49ba
micah_z <micah.a.zahn@gmail.com>
2022-06-07 09:31:29 -0500

    add 'add comment' functionality

---
+++ public/javascript/comment.js
@@ -0,0 +1,32 @@
+async function newCommentFormHandler(event) {
+    event.preventDefault();
+
+    const urlArray = window.location.toString().split('/');
+    const review_id = urlArray[urlArray.length - 1];
+  
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value;
+
+
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        comment_text,
+        review_id,    
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    console.log(response)
+    
+  
+    if (response.ok) {
+    //   document.location.replace('/dashboard');
+        document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.comment-form').addEventListener('submit', newCommentFormHandler);
